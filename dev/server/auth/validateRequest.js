655bdc00faa60b96d9263f86cf1108d18c039bd9
hindsricardo <hindsricardo@gmail.com>
2015-11-06 23:38:22 -0600

    updating dev server

--- dev/server/auth/validateRequest.js
+++ dev/server/auth/validateRequest.js
@@ -6,6 +6,7 @@ var isEmailValid = function (db, email, callback) {
     });
 };
 
+
 module.exports.validate = function (req, res, db, callback) {
     // if the request dosent have a  header with email, reject the request
     if (!req.params.token) {
@@ -21,13 +22,16 @@ module.exports.validate = function (req, res, db, callback) {
 
     isEmailValid(db, req.params.token, function (user) {
         if (!user) {
+
             res.writeHead(403, {
                 'Content-Type': 'application/json; charset=utf-8'
             });
             res.end(JSON.stringify({
                 error: "You are not authorized to access this application",
-                message: "Invalid User Email"
+                message: "Invalid User Email or Username"
             }));
+
+
         } else {
             callback();
         }
